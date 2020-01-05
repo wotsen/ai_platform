9ca16bbf209637b303e005e864de518360c251a4
ywl <astralrovers@outlook.com>
2020-01-05 15:57:02 +0800

    修改sdk协议更新方式

--- protocol/insider/sdk/compile.sh
+++ protocol/insider/sdk/compile.sh
@@ -15,5 +15,11 @@ echo $protos
 
 ./protoc --proto_path=./ --cpp_out=sdk_cpp_code --python_out=sdk_py_code --js_out=sdk_js_code $protos
 
-#cp sdk_cpp_code/* ../../../service/app/base_fun/sdk_net/sdk_protocol/
-#cp sdk_cpp_code/* ../../../service/test/sdk_test/sdk_protocol/
+cp sdk_cpp_code/* ../../../server/src/base_fun/sdk_net/sdk_protocol/
+cp *.proto ../../../server/src/base_fun/sdk_net/sdk_protocol/proto3/
+
+cp sdk_py_code/*.py ../../../client/src/sdknet/sdk_protocol/
+cp *.proto ../../../client/src/sdknet/sdk_protocol/proto3/
+
+cp sdk_js_code/*.js ../../../website/src/sdknet/sdk_protocol/
+cp *.proto ../../../website/src/sdknet/sdk_protocol/proto3/
